9f32d294b54a7ea92f913dfc00a2ccb642c84e8d
JigarRajput <rajputjigar455@gmail.com>
2024-12-27 01:36:34 +0530

    Deployment changes for vercel

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -4,10 +4,11 @@ const path = require('path');
 let db;
 
 async function getDb() {
+  console.log(path.join(__dirname, 'database.sqlite'))
   // If db is not initalized, initialize it by opening a new connection.
   if (!db) {
     db = await open({
-      filename: path.join(__dirname, 'database', 'database.sqlite'),
+      filename: path.join(__dirname, 'database.sqlite'),
       driver: sqlite3.Database,
     });
   }
--- database/initDB.js
+++ database/initDB.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
-
+const path = require('path');
 // Connect to SQLite database
-const db = new sqlite3.Database(path.join(__dirname, 'database', 'database.sqlite'), (err) => {
+const db = new sqlite3.Database(path.join(__dirname, 'database.sqlite'), (err) => {
   if (err) {
     console.error('Error opening database:', err.message);
   } else {
