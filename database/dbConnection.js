f945c82bd68d0721317d13f3b5a3656bbba1c340
JigarRajput <rajputjigar455@gmail.com>
2024-12-27 01:16:20 +0530

    Deployment for database not accesible issue

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,12 +1,13 @@
 const { open } = require('sqlite');
 const sqlite3 = require('sqlite3');
+const path = require('path');
 let db;
 
 async function getDb() {
   // If db is not initalized, initialize it by opening a new connection.
   if (!db) {
     db = await open({
-      filename: './database/database.sqlite',
+      filename: path.join(__dirname, 'database', 'database.sqlite'),
       driver: sqlite3.Database,
     });
   }
